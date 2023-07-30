bbfbf6d04e8736a07a8ca8a727c5318b3141035e
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-07-30 08:33:08 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basics - Home</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicon/site.webmanifest">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="topnav">
+        <h2>PHP Basics</h2>
+        <p>All course files and examples</p>
+    </div>
+    <div class="quicklinks">
+        <ul>
+            <li><a href=""><button>Learn PHP</button></a></li>
+            <li><a href=""><button>Install PHP</button></a></li>
+            <li><a href=""><button>Constants</button></a></li>
+            <li><a href=""><button>Data Types</button></a></li>
+            <li><a href=""><button>Boolean</button></a></li>
+            <li><a href=""><button>Integer</button></a></li>
+            <li><a href=""><button>Float</button></a></li>
+            <li><a href=""><button>Integer</button></a></li>
+            <li><a href=""><button>String</button></a></li>
+            <!-- <li><a href=""><button>Null</button></a></li>
+            <li><a href=""><button>Array</button></a></li>
+            <li><a href=""><button>Expressions</button></a></li>
+            <li><a href=""><button>Operators <br> Part 1</button></a></li>
+            <li><a href=""><button>Operators <br> Part 2</button></a></li>
+            <li><a href=""><button>Operator Precedence</button></a></li>
+            <li><a href=""><button>Conditional Statement</button></a></li>
+            <li><a href=""><button>Loops</button></a></li>
+            <li><a href=""><button>Switch</button></a></li>
+            <li><a href=""><button>Match Expressions</button></a></li> -->
+        </ul>
+    </div>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
